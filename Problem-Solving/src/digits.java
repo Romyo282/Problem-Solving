381dcf2d85e00a1af1f125d4da66f4f1e25f7da6
DELL <m.ramadan88@gmail.com>
2024-04-29 04:26:18 +0300

    Factorial Problem

---
+++ Problem-Solving/src/digits.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+//Reverse the number which user enter.
+public class digits {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("enter size of num  : ");
+        int size = s.nextInt();
+
+        for (int i = 0; i <size; i++){
+            System.out.print("\nenter value : ");
+            int value = s.nextInt();
+            while (value != 0)
+            {
+                int num = value % 10;
+                System.out.print(num + " ");
+                value /= 10;
+            }
+
+        }
+    }
+}
