28fbdc38aa79d0286d7e32fae72d450bba35068c
DELL <m.ramadan88@gmail.com>
2024-04-29 03:14:29 +0300

    Factorial Problem

--- Problem-Solving/src/Factorial.java
+++ Problem-Solving/src/Factorial.java
@@ -3,18 +3,23 @@ import java.util.Scanner;
 public class Factorial {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        System.out.print("Enter num : ");
         long number = s.nextLong();
-        long result = fact(number);
-
-        System.out.println(result);
-
-
-    }
-    public static long fact(long num) {
-        if (num == 0) return 1;
+//        long result = fact(number);
+        if (number == 1 || number == 0){
+            System.out.println("no");
+        }
         else
-            return num * fact(num - 1);
+            System.out.println("yes");
+
+
+
     }
+//    public static long fact(long num) {
+//        if (num == 0) return 1;
+//        else
+//            return num * fact(num - 1);
+//    }
 }
 
 
